85257b68b441daedf9b10504b6ecf9be59ed56d8
Maksim Tihomirov <maxbeere@yandex.ru>
2021-05-16 23:45:23 +0300

    feat: create enum

---
+++ src/resources/users/user.enum.js
@@ -0,0 +1,9 @@
+const MESSAGE = {
+  NotFound: 'User not found.',
+  UserCreated: 'The user has been created.',
+  UserUpdated: 'The user has been updated.',
+  BadRequset: 'Bad request',
+  UserDeleted: 'The user has been deleted'
+}
+
+module.exports = MESSAGE;
